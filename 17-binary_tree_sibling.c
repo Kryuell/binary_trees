915d9843bc752ef37b3a132895b6a5aaf304de9b
Kryuell <Kryuel17@gmail.com>
2024-02-28 18:31:58 +0100

    Binary trees

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_sibling - function that finds if a node is sibling
+* @node: node to check
+* Return: The sibling node
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
