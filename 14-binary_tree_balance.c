b7e2df093d778c19fc95530b900debf53ff67168
Kryuell <Kryuel17@gmail.com>
2024-02-28 18:15:18 +0100

    Binary trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,39 +1,46 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_height - measures the height of a binary tree
-* @tree: pointer to the root node of the tree to measure the height
-*
-* Return: height of the tree
-*/
-size_t binary_tree_height_2(const binary_tree_t *tree)
+ * binary_tree_height_b - Measures height of a binary tree for a bal tree
+ * @tree: tree to go through
+ * Return: the height
+ */
+
+size_t binary_tree_height_b(const binary_tree_t *tree)
 {
 	size_t l = 0;
 	size_t r = 0;
 
 	if (tree == NULL)
+	{
 		return (0);
-
-	l = tree->left ? 1 + binary_tree_height_2(tree->left) : 0;
-	r = tree->right ? 1 + binary_tree_height_2(tree->right) : 0;
-
-	return ((l > r) ? l : r);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+			r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+		}
+		return ((l > r) ? l : r);
+	}
 }
 
 /**
-* binary_tree_balance - measures the balance factor of a binary tree
-* @tree: pointer to the root node of the tree to measure the balance factor
-*
-* Return: balance factor or 0 if tree is NULL
-*/
+ * binary_tree_balance - Measures balance factor of a binary tree
+ * @tree: tree to go through
+ * Return: balanced factor
+ */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	int right = 0, left = 0, total = 0;
 
-	int left_height = tree->left ? (int)binary_tree_height(tree->left) : -1;
-
-	int right_height = tree->right ? (int)binary_tree_height(tree->right) : -1;
-
-	return (left_height - right_height);
+	if (tree)
+	{
+		left = ((int)binary_tree_height_b(tree->left));
+		right = ((int)binary_tree_height_b(tree->right));
+		total = left - right;
+	}
+	return (total);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -6,10 +6,11 @@
 /* Basic Binary Tree */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 
@@ -22,8 +23,11 @@ typedef struct binary_tree_s avl_t;
 /* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
