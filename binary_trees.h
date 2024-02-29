8202dddcbd4869f1e99f71fea7d9b5aa521f3d56
mounimnadir7@gmail.com <mounimnadir7@gmail.com>
2024-02-29 07:06:44 -0800

    binary_trees_levelorder

--- binary_trees.h
+++ binary_trees.h
@@ -1,4 +1,7 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
@@ -12,6 +15,17 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
 typedef struct binary_tree_s binary_tree_t;
 
 /* Binary Search Tree */
@@ -46,5 +60,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
 
