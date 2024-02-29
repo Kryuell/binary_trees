0102e5aae7279cfa7fa552361788c37ca7211291
mounimnadir7@gmail.com <mounimnadir7@gmail.com>
2024-02-29 08:15:30 -0800

    updated

--- binary_trees.h
+++ binary_trees.h
@@ -13,10 +13,8 @@ struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
+};
+typedef struct binary_tree_s binary_tree_t;
 
 typedef struct levelorder_queue_s
 {
